00eb7c83bfd6be6c2a7613a1f8112c9c16e51752
dwilliams64 <dwilliams_64@yahoo.com>
2018-12-03 17:35:28 -0600

    Started creating Card component

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Card = () => {
+    return(        
+        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img src='https://robohash.org/test?200x200' />
+            <h2>Barry Burtton</h2>
+            <p>Barry Burtton.com</p>
+        </div>
+    );
+}
+
+export default Card;

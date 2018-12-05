84ba616b3872f10a6e0c71d34f2dc44cc47229f3
dwilliams64 <dwilliams_64@yahoo.com>
2018-12-04 18:30:50 -0600

    Finished card component

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = (e) => {
+    console.log(e);
     return(        
-        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img src='https://robohash.org/test?200x200' />
-            <h2>Barry Burtton</h2>
-            <p>Barry Burtton.com</p>
+        <div className="bg-light-green tc dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img alt="robot face" src={`https://robohash.org/${e.id}?200x200`} />
+            <h2>{e.name}</h2>
+            <p>{e.email}</p>
         </div>
     );
 }

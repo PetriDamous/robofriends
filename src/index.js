3292d638112d012c6b8d34a09bcd5405807361c4
dwilliams64 <dwilliams_64@yahoo.com>
2018-12-05 16:09:47 -0600

    trying to create randomizer

--- src/index.js
+++ src/index.js
@@ -5,12 +5,26 @@ import Card from './Card';
 import 'tachyons';
 import * as serviceWorker from './serviceWorker';
 import { robots } from './robots';
+let ranNum = Math.floor(Math.random() * 6);
+
+
+/*
+Array.prototype.shuffle = function(){
+    for(i = this.length-1; i > 0; i--){
+        let j = Math.floor(Math.random()*(i+1));
+        let temp = this[j];
+        this[j] = this[i];
+        this[i] = temp;
+    }
+    return this;
+}
+*/
 
 ReactDOM.render(
     <div>
-        <Card id={ robots[0].id } name={robots[0].name } email={ robots[0].email } />
-        <Card id={ robots[1].id } name={ robots[1].name } email={ robots[1].email } />
-        <Card id={ robots[2].id } name={ robots[2].name } email={ robots[2].email } />
+        <Card id={ robots[ranNum].id } name={robots[ranNum].name } email={ robots[ranNum].email } />
+        <Card id={ robots[ranNum].id } name={ robots[ranNum].name } email={ robots[ranNum].email } />
+        <Card id={ robots[ranNum].id } name={ robots[ranNum].name } email={ robots[ranNum].email } />
     </div>
 
 , document.getElementById('root'));

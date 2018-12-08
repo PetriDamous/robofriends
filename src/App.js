c6f5c482ace78a39319a7b5d377e8f6f1d962006
dwilliams64 <dwilliams_64@yahoo.com>
2018-12-08 16:07:23 -0600

    Created App component to handle all compoents inside of app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { robots } from './robots';
+import CardList from './CardList';
+
+const App = () => {
+    return(
+        <div>
+            <h1>Robofriends</h1>
+            <CardList robots={robots} />
+        </div>
+        
+    );
+}
+
+export default App;

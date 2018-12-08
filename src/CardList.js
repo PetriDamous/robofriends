337de018dcccd5987c9516162274af08d6a50496
dwilliams64 <dwilliams_64@yahoo.com>
2018-12-08 16:23:35 -0600

    refactored CardList code

--- src/CardList.js
+++ src/CardList.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Card from './Card';
 
-const CardList = ({ robots }) => {
-    const cardArray = robots.map(user => {
-        return <Card key={ user.id } id={ user.id } name={ user.name } email={ user.email } />        
-    });    
+const CardList = ({ robots }) => {      
       
     return(
         <div>
-            { cardArray }
+            {
+                robots.map(user => {
+                    return <Card key={ user.id } id={ user.id } name={ user.name } email={ user.email } />        
+                })
+            }
         </div>
     );     
 }
